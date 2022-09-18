08565d15fa2aedb1db460599a0c85711076a2404
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-09-18 14:47:55 +0530

    Add files via upload

---
+++ zigzagpattern.cpp
@@ -0,0 +1,17 @@
+// for solving pattern use their index 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c,r;
+    cin>>r>>c;
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=c;j++){
+            if ((i+j)%4==0 || (i==2 && j%4==0))
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
