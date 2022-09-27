b130bf9c5180084bf6521b47932f519c3faed986
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-09-28 00:00:34 +0530

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,22 @@
+
+// #include<iostream>
+// using namespace std;
+// typedef long long ll;
+// int main(){
+//     int p;
+//     while(p--){
+//         ll n,k;cin>>n>>k;
+//         // int a=n,b=1;
+//         k--;
+//         if(n%2==0)
+//         cout<<k%n+1<<"\n";
+//         else{
+//             int col=k/(n/2);
+//             cout<<(k+col)%n+1<<"\n";
+//         }
+        
+//     cout<<b;
+//     }
+//     return 0;
+// }
+
