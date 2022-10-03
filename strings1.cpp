99726143706daf94a34c62aa350e0c748e1abec3
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-10-03 19:05:01 +0530

    Add files via upload

---
+++ strings1.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    // string str;
+    // string s(5,'n');  //print n 5 times
+    // cout<<s<<endl;
+
+    // // getline(cin,str);
+    // // cout<<str<<endl;
+
+
+    // string s1="abc";
+    // string s2="def";
+
+    // cout<<s1+s2<<endl;
+
+    // s1.append(s2);  //adding s2 in s1
+
+    // string s3="xyz";
+    // cout<<s3.compare(s1)<<endl;
+
+    // s1.clear();
+
+    // if(s1.empty())
+    // cout<<"empty"<<endl;
+
+    // string s1="ravikantsharma";
+    // s1.erase(3,5);   //for erasiing the no of characters  (place, no. of characters)
+    // cout<<s1<<endl;
+
+
+
+    // string ss="nincompoop";
+    // ss.find("p");   //returns the first index of the finding element
+    // ss.insert(2,"ravi");   //inserts the element at the specified position
+
+
+    // ss.length();   //returns the size of the string 
+
+
+    // string rr="786";
+    // int r=stoi(rr);  //converting string to int
+    
+
+    // string r="68yujhjh";
+    // cout<<r.substr(1);   //creates a string from the given index
+    
+    // int x=99;
+    // cout<< to_string(x) + "rr" <<endl;  //for converting number ti string
+    return 0;
+}
